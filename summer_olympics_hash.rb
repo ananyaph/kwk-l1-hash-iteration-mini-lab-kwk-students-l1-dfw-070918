2f8002c83caf4816505914a5bfa70b88ff1e2d64
Ananya Phadke <phadkeananya@gmail.com>
2018-07-11 18:54:58 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -9,6 +9,12 @@ def create_olympics_hash
 end
 
 def add_a_key_value_pair
+  summer_olympics = {
+  :Sydney => "2000", 
+  :Athens => "2004", 
+  :Beijing => "2008", 
+  :London => "2012"
+}
   summer_olympics = [:Atlanta] = "1996"
 end
 
