7b2ac326090923909ce9bf9846ef044b0f37a778
Ananya Phadke <phadkeananya@gmail.com>
2018-07-11 19:06:38 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -29,7 +29,7 @@ def iterate_through_hash
 summer_olympics[:Atlanta] = "1996"
 
   summer_olympics.each do |location, year|
-    puts "The #{year} summer olympics took place in #{location}"
+    puts "The #{location} summer olympics took place in #{year}."
   end
 end
 
